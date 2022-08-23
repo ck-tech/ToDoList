906db43bc1487efb8ea5891909845cf59830b2b7
CHIGUCHI Katsuya <66811170+ck-tech@users.noreply.github.com>
2022-08-23 17:30:58 +0900

    update.

--- src/index.js
+++ src/index.js
@@ -15,21 +15,38 @@ const onClickAdd = () => {
   // buttom (完了) タグの作成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // クリックされた完了ボタンの親要素（div）を未完了リストから削除
+    deleteFromIncompleteList(completeButton.parentNode);
+
+    // 完了したTODOに追加
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    div.removeChild(completeButton);
+    div.removeChild(deleteButton);
+    div.appendChild(backButton);
+    document.getElementById("complete-list").appendChild(div);
+  });
 
   // buttom (削除) タグの作成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // クリックされた削除ボタンの親要素（div）を未完了リストから削除
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
 
   // divの子要素に各要素を設定
   div.appendChild(li);
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
-  console.log(div);
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(div);
+};
 
-  //alert(inputText);
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document.getElementById("add-button").addEventListener("click", () => {
